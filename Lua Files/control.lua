b6d80f9e0a3608897a5a420a9664938f9238a483
theis999 <theis99999@gmail.com>
2024-07-28 08:40:35 +0200

    Fix issue with put when taking from multiple stacks

--- Lua Files/control.lua
+++ Lua Files/control.lua
@@ -257,30 +257,35 @@ local function put()
 		return false
 	end
 
-	local item_stack = global.tas.player.get_main_inventory().find_item_stack(global.tas.item)
-	if not item_stack then Error("Item stack "..global.tas.item.." not found for put") end
-	local health, durability, ammo = item_stack.health, item_stack.is_tool and item_stack.durability or 1, item_stack.is_ammo and item_stack.ammo or 10
+	local c = global.tas.amount
+	while c > 0 do
+		local item_stack = global.tas.player.get_main_inventory().find_item_stack(global.tas.item)
+		if not item_stack then Error("Item stack "..global.tas.item.." not found for put") return false end
+		local health, durability, ammo = item_stack.health, item_stack.is_tool and item_stack.durability or 1, item_stack.is_ammo and item_stack.ammo or 10
+		local count = global.tas.amount < item_stack.count and global.tas.amount or item_stack.count
+		c = c - count
 
-	global.tas.amount = global.tas.target_inventory.insert{
-		name=global.tas.item,
-		count=global.tas.amount,
-		health=health,
-		durability=durability,
-		ammo=ammo,
-	}
+		global.tas.amount = global.tas.target_inventory.insert{
+			name=global.tas.item,
+			count=count,
+			health=health,
+			durability=durability,
+			ammo=ammo,
+		}
 
-	if global.tas.amount < 1 then
-		Warning(string.format("Step: %s, Action: %s, Step: %d - Put: %s can not be transferred. Amount: %d Removalable: %d Insertable: %d", global.tas.task[1], global.tas.task[2], global.tas.step, global.tas.item:gsub("-", " "):gsub("^%l", string.upper), global.tas.amount, removalable_items, insertable_items))
-		return false
+		if global.tas.amount < 1 then
+			Warning(string.format("Step: %s, Action: %s, Step: %d - Put: %s can not be transferred. Amount: %d Removalable: %d Insertable: %d", global.tas.task[1], global.tas.task[2], global.tas.step, global.tas.item:gsub("-", " "):gsub("^%l", string.upper), global.tas.amount, removalable_items, insertable_items))
+			return false
+		end
+
+		global.tas.amount = global.tas.player.remove_item{
+			name=global.tas.item,
+			count=count,
+			health=health,
+			durability=durability,
+			ammo=ammo,
+		}
 	end
-
-	global.tas.amount = global.tas.player.remove_item{
-		name=global.tas.item,
-		count=global.tas.amount,
-		health=health,
-		durability=durability,
-		ammo=ammo,
-	}
 
 	local text = string.format("-%d %s (%d)", global.tas.amount, format_name(global.tas.item), global.tas.player.get_item_count(global.tas.item)) --"-2 Iron plate (5)"
 	local pos = {x = global.tas.target_inventory.entity_owner.position.x + #text/2 * font_size, y = global.tas.target_inventory.entity_owner.position.y }
@@ -308,14 +313,14 @@ local function take_all()
 	local contents = global.tas.target_inventory.get_contents()
 	for name, count in pairs(contents or global.tas.target_inventory) do
 		local item_stack = global.tas.target_inventory.find_item_stack(name)
-		local durability = 1
-		if item_stack.is_tool then
-			durability = item_stack.durability
-		end
-
+		if not item_stack then Error("Item stack "..global.tas.item.." not found for put") return false end
+		local health, durability, ammo = item_stack.health, item_stack.is_tool and item_stack.durability or 1, item_stack.is_ammo and item_stack.ammo or 10
+		
 		global.tas.amount = global.tas.player.insert{
 			name=name,
 			durability=durability,
+			health=health,
+			ammo=ammo,
 			count=global.tas.target_inventory.remove{name=name, count=count, durability=durability}
 		}
 
@@ -376,15 +381,15 @@ local function take()
 	end
 
 	local item_stack = global.tas.target_inventory.find_item_stack(global.tas.item)
-	local durability = 1
-	if item_stack.is_tool then
-		durability = item_stack.durability
-	end
+	if not item_stack then Error("Item stack "..global.tas.item.." not found for put") return false end
+	local health, durability, ammo = item_stack.health, item_stack.is_tool and item_stack.durability or 1, item_stack.is_ammo and item_stack.ammo or 10
 
 	global.tas.amount = global.tas.player.insert{
 		name=global.tas.item,
 		durability=durability,
-		count=global.tas.target_inventory.remove{name=global.tas.item, count=global.tas.amount, durability=durability}
+		health=health,
+		ammo=ammo,
+		count=global.tas.target_inventory.remove{name=global.tas.item, count=global.tas.amount, durability=durability, health=health, ammo=ammo}
 	}
 
 	local text = string.format("+%d %s (%d)", global.tas.amount, format_name(global.tas.item), global.tas.player.get_item_count(global.tas.item)) --"+2 Iron plate (5)"
