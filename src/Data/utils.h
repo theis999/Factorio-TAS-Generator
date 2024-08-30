d3aeaafdafa96b1c94aae7108b93b3c49fc303cf
theis999 <theis99999@gmail.com>
2024-08-30 07:10:37 +0200

    Update version to 0.6.0

--- src/Data/utils.h
+++ src/Data/utils.h
@@ -15,7 +15,7 @@ static const double invalidX = 0.3543534;
 static const struct GeneratorThumbprint
 {
 	string name = "Factorio TAS Generator";
-	string version = "0.5.7";
+	string version = "0.6.0";
 } generator_thumbprint;
 
 static const vector<string> fuel_list = {
