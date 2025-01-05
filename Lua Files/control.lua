e7dcc1952451d9e90f0c0679646d851aa5933634
theis999 <theis99999@gmail.com>
2025-01-05 13:17:54 +0100

    Changed to using build from cursor to build buildings

--- Lua Files/control.lua
+++ Lua Files/control.lua
@@ -518,7 +518,6 @@ local function create_entity_replace()
 		Error("Trying to create an entity of "..storage.tas.item.." but couldn't find an stack of them in players inventory")
 		return false
 	end
-	--[[ Using player.build_from_cursor
 	
 	storage.tas.player.clear_cursor()
 	storage.tas.player.cursor_stack.swap_stack(stack)
@@ -538,95 +537,7 @@ local function create_entity_replace()
 		end
 
 		return false
-	end]]
-
-	local fast_replace_type_lookup = {
-		["underground-belt"] = {"transport-belt", "fast-transport-belt", "express-transport-belt"},
-		["fast-underground-belt"] = {"transport-belt", "fast-transport-belt", "express-transport-belt"},
-		["express-underground-belt"] = {"transport-belt", "fast-transport-belt", "express-transport-belt"},
-		["pipe-to-ground"] = {"pipe"}
-	}
-
-	local created_entity = storage.tas.player.surface.create_entity{name = storage.tas.item, position = storage.tas.target_position, direction = storage.tas.direction, force="player", fast_replace=true, player=storage.tas.player, raise_built = true, item = stack}
-
-	if created_entity and fast_replace_type_lookup[created_entity.name] ~= nil and created_entity.neighbours  then --connected entities eg underground belt https://lua-api.factorio.com/latest/LuaEntity.html#LuaEntity.neighbours
-		created_entity.create_build_effect_smoke()
-		created_entity.surface.play_sound{path="entity-build/"..created_entity.prototype.name, position=created_entity.position}
-
-		created_entity.health = stack.health * created_entity.health
-
-		local replace_type = fast_replace_type_lookup[created_entity.name]
-		local neighbour_position = nil
-		if (#created_entity.neighbours == 0) then 
-			neighbour_position = created_entity.neighbours.position
-		else
-			for _,neighbour in pairs(created_entity.neighbours[1]) do
-				if (created_entity.name == neighbour.name) then
-					neighbour_position = neighbour.position
-				end
-			end
-		end
-		if (not neighbour_position) then
-			storage.tas.player.remove_item({name = storage.tas.item, count = 1})
-			end_warning_mode(string.format("Step: %s, Action: %s, Step: %d - Build: [item=%s]", storage.tas.task[1], storage.tas.task[2], storage.tas.step, storage.tas.item ))
-			return true
-		end
-
-		local entities_between = storage.tas.player.surface.find_entities_filtered{name = replace_type, area = {
-			{x=math.min(created_entity.position.x, neighbour_position.x), y=math.min(created_entity.position.y, neighbour_position.y)},
-			{x=math.max(created_entity.position.x, neighbour_position.x), y=math.max(created_entity.position.y, neighbour_position.y)}}}
-		local entities_between_length = math.abs(created_entity.position.x - neighbour_position.x + created_entity.position.y - neighbour_position.y) - 1
-		local can_replace_all = entities_between_length == #entities_between
-
-		--chech that all entities betweeen are in the same direction
-		if can_replace_all and created_entity.name ~= "pipe-to-ground" then --ignore direction for pipes
-			for __, e in pairs(entities_between) do
-				if e.direction ~= created_entity.direction then
-					can_replace_all = false
-					break
-				end
-			end
-		elseif can_replace_all and created_entity.name == "pipe-to-ground" then
-			for __, e in pairs(entities_between) do --check all entities
-				if e.neighbours and e.neighbours[1] then -- null check
-					for ___, n in pairs(e.neighbours[1]) do --check all neighbours for each entity
-						for i = 1, #entities_between do --make sure it exist every neighbour is only part of the set of entities between
-							if entities_between[i] == n then
-								can_replace_all = true
-								break --break out when found
-							else
-								can_replace_all = false
-							end
-						end
-						if not can_replace_all then break end --previous loop didn't find it
-					end
-				end
-				if not can_replace_all then break end --break out
-			end
-		end
-		--mine all entities inbetween
-		if can_replace_all then
-			for __, e in pairs(entities_between) do
-				storage.tas.player.mine_entity(e, true)
-			end
-		end
-		--spend the item placed
-		storage.tas.player.remove_item({name = storage.tas.item, count = 1})
-		end_warning_mode(string.format("Step: %s, Action: %s, Step: %d - Build: [item=%s]", storage.tas.task[1], storage.tas.task[2], storage.tas.step, storage.tas.item ))
-		return true
 	end
-
-	--no special fast replace handling
-	if created_entity then
-		created_entity.create_build_effect_smoke()
-		created_entity.surface.play_sound{path="entity-build/"..created_entity.prototype.name, position=created_entity.position}
-
-		created_entity.health = stack.health * created_entity.health
-
-		end_warning_mode(string.format("Step: %s, Action: %s, Step: %d - Build: [item=%s]", storage.tas.task[1], storage.tas.task[2], storage.tas.step, storage.tas.item ))
-		storage.tas.player.remove_item({name = storage.tas.item, count = 1})
-	end
-	return created_entity ~= nil
 end
 
 -- Creating buildings
